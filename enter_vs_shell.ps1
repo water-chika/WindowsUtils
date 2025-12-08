f036f2befae82810c5a2d096cd9f12f13d6170b4
Water Chika <water_chika@outlook.com>
2025-12-09 00:05:11 +0800

    split enter_vs_shell.ps1

---
+++ enter_vs_shell.ps1
@@ -0,0 +1,2 @@
+Import-Module "C:\Program Files (x86)\Microsoft Visual Studio\18\BuildTools\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
+Enter-VsDevShell a68e13d3 -SkipAutomaticLocation -DevCmdArguments "-arch=x64 -host_arch=x64"
--- powershell_setting.ps1
+++ powershell_setting.ps1
@@ -1,3 +1,1 @@
 Set-PSReadlineOption -Edit "Emacs"
-Import-Module "C:\Program Files (x86)\Microsoft Visual Studio\18\BuildTools\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
-Enter-VsDevShell a68e13d3 -SkipAutomaticLocation -DevCmdArguments "-arch=x64 -host_arch=x64"
