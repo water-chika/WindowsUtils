55dd62e8c3b4a604aa402f6fb33a26478a5fcf4c
Water Chika <water_chika@outlook.com>
2025-12-09 12:03:36 +0800

    update

--- enter_vs_shell.ps1
+++ enter_vs_shell.ps1
@@ -1,2 +1,3 @@
-Import-Module "C:\Program Files (x86)\Microsoft Visual Studio\18\BuildTools\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
-Enter-VsDevShell a68e13d3 -SkipAutomaticLocation -DevCmdArguments "-arch=x64 -host_arch=x64"
+$VsInstallPath = "C:\Program Files (x86)\Microsoft Visual Studio\18\BuildTools"
+Import-Module "$VsInstallPath\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
+Enter-VsDevShell -InstallPath $VsInstallPath -SkipAutomaticLocation -DevCmdArguments "-arch=x64 -host_arch=x64"
