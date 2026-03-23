a3c01d35222b853a2a41cfca60f7ba60a4580b6c
Water Chika <water_chika@outlook.com>
2026-03-23 23:24:38 +0800

    fix water_config.ps1

--- water_config.ps1
+++ water_config.ps1
@@ -1,6 +1,6 @@
-$PSScriptRoot/add_path.ps1 .
-$PSScriptRoot/customize_powershell.ps1
-$PSScriptRoot/enable_sshd.ps1
-$PSScriptRoot/hide_recycle_bin.ps1
-set_chinese_dvorak_layout.ps1
+& $PSScriptRoot/add_path.ps1 .
+& $PSScriptRoot/customize_powershell.ps1
+& $PSScriptRoot/enable_sshd.ps1
+& $PSScriptRoot/hide_recycle_bin.ps1
+& $PSScriptRoot/set_chinese_dvorak_layout.ps1
 
