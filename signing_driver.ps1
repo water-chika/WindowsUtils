84a39ca6c045d19e38f0e544a14b502351a44351
Water Chika <water_chika@outlook.com>
2026-04-08 22:17:12 +0800

    add signing_driver.ps1

--- signing_driver.ps1
+++ signing_driver.ps1
@@ -1,13 +1,20 @@
 param(
-    $File,
+    $DriverDir,
+    $CatFile,
     $Host
 )
+
+Inf2Cat.exe /driver:$DriverDir /os:Vista_x64
+
 MakeCert -r -pe -ss PrivateCertStore -n "CN=test.com(Test)" testcert.cer
 Signtool sign /v /fd sha256 /s PrivateCertStore /n 'Test.com(Test)' /t http://timestamp.digicert.com $File
 
 Get-Command certmgr.exe | Copy-Item
 ssh $Host "mkdir C:/Debug"
 scp certmgr.exe $Host:C:/Debug
-ssh $Host "C:/Debug/certmgr.exe /add C:/Debug/testcert.cer /s /r localMachine root"
-ssh $Host "certutil.exe -store root"
+
+$cert_store = "TrustedPublisher"
+ssh $Host "C:/Debug/certmgr.exe /add C:/Debug/testcert.cer /s /r localMachine $cert_store"
+
+ssh $Host "certutil.exe -store $cert_store"
 
