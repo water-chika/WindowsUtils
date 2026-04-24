705d7e4f9cb82b775a2133a0c3d4f87dc0305d01
Water Chika <water_chika@outlook.com>
2026-04-24 20:29:13 +0800

    fix typo

--- enable_dev_mode.ps1
+++ enable_dev_mode.ps1
@@ -1,4 +1,4 @@
-$app_model_unlock = "HKLM:/Software/Mircosoft/Windows/CurrentVersion/AppModelUnlock"
+$app_model_unlock = "HKLM:/Software/Microsoft/Windows/CurrentVersion/AppModelUnlock"
 New-ItemProperty -Path $app_model_unlock -Name 'AllowAllTrustedApp' -PropertyType DWord -Value 1
 New-ItemProperty -Path $app_model_unlock -Name 'AllowDevelopmentWithoutDevLicense' -PropertyType DWord -Value 1
 
