9ed4b0cd47f8d07a0afe101716f7819ab78e9c65
Water Chika <water_chika@outlook.com>
2026-06-02 15:28:37 +0800

    customize_powershell.ps1: create path parent dir

--- customize_powershell.ps1
+++ customize_powershell.ps1
@@ -1,1 +1,6 @@
+$destination = $PROFILE.CurrentUserAllHosts
+$directory = Split-Path --parent $destination
+while (-not (Get-Item $directory)) {
+	New-Item -ItemType Directory $directory
+}
 Copy-Item powershell_profile.ps1 $PROFILE.CurrentUserAllHosts
