17348c4f6f6d3e2a3cde3b0e031906874b43f923
Water Chika <water_chika@outlook.com>
2026-06-02 15:33:16 +0800

    customize_powershell.ps1: fix bug

--- customize_powershell.ps1
+++ customize_powershell.ps1
@@ -1,6 +1,7 @@
 $destination = $PROFILE.CurrentUserAllHosts
-$directory = Split-Path --parent $destination
+$directory = Split-Path -Parent $destination
 while (-not (Get-Item $directory)) {
 	New-Item -ItemType Directory $directory
+	$directory = Split-Path --parent $directory
 }
 Copy-Item powershell_profile.ps1 $PROFILE.CurrentUserAllHosts
